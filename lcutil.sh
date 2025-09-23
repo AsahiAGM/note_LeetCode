d7ef1419cca871142649dd540ad8634579824f64
Asahi Hamaguchi <130739802+AsahiAGM@users.noreply.github.com>
2025-09-23 16:15:10 +0900

    cp folder

--- lcutil.sh
+++ lcutil.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 setlang () 
 { 
+    echo "  setlang..."
     lang="$1"
     probname="$2"
     cd "$lang" && touch "main.$lang" "$probname.$lang"
 }
 solution () 
 { 
+    echo "  solution..."
     cd problems
     mkdir "$1" && cd "$1" && mkdir c cpp cs py rust go
 }
 fsolution()
 {
-    cd problems
-    mkdir "$1" && cd "$1" && mkdir c cpp cs py rust go
+    echo "fsolution..."
+    solution "$1"
     setlang "$2" "$3"
+    cd ../../..
 }
